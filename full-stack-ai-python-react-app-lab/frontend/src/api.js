c8b7b0780a98599c1e16e50b6a3b62a7452debe5
JaeceeNaylor <jaecee.grant@gmail.com>
2025-08-12 19:13:43 -0600

    update vite

--- full-stack-ai-python-react-app-lab/frontend/src/api.js
+++ full-stack-ai-python-react-app-lab/frontend/src/api.js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 export async function sendPrompt(prompt) {
   try {
-    const res = await axios.post("http://localhost:8000/analyze", { prompt });
+    const res = await axios.post(
+        "/api/analyze",
+        { prompt },
+        {
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        }
+  );
     return res.data;
   } catch (error) {
     return { error: error.message };
--- full-stack-ai-python-react-app-lab/frontend/vite.config.js
+++ full-stack-ai-python-react-app-lab/frontend/vite.config.js
@@ -9,6 +9,21 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
-     allowedHosts: ["sharing-glider-ualf--3000.pluralsight.run", "http://localhost:3000/", "http://0.0.0.0:3000/"],
+    allowedHosts: ["sharing-glider-ualf--3000.pluralsight.run", "http://localhost:3000/", "http://0.0.0.0:3000/"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: (proxy) => {
+          proxy.on("proxyReq", (proxyReq) => {
+            proxyReq.setHeader("Origin", "http://localhost:3000"); // Override the origin
+          });
+        },
+        headers: {
+          "Access-Control-Allow-Credentials": "true",
+        },
+      },
+    },
   },
 });
