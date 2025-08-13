17c273a7623641953075c2b12e1fe8d288ca4552
JaeceeNaylor <jaecee.grant@gmail.com>
2025-08-12 18:10:05 -0600

    update vite config

--- full-stack-ai-python-react-app-lab/frontend/vite.config.js
+++ full-stack-ai-python-react-app-lab/frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vite.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+     allowedHosts: ["sharing-glider-ualf--3000.pluralsight.run", "http://localhost:3000/", "http://0.0.0.0:3000/"],
+  },
+});
