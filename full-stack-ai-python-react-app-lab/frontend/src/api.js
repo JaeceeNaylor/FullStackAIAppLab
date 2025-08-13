acca6c754b7b56e590a9dfd5eb3152aa25271c9e
JaeceeNaylor <jaecee.grant@gmail.com>
2025-08-13 10:43:41 -0600

    starting state

--- full-stack-ai-python-react-app-lab/frontend/src/api.js
+++ full-stack-ai-python-react-app-lab/frontend/src/api.js
@@ -1,18 +1,5 @@
 import axios from "axios";
 
 export async function sendPrompt(prompt) {
-  try {
-    const res = await axios.post(
-        "/api/analyze",
-        { prompt },
-        {
-            headers: {
-            'Content-Type': 'application/json',
-            },
-        }
-  );
-    return res.data;
-  } catch (error) {
-    return { error: error.message };
-  }
+    return;
 }
